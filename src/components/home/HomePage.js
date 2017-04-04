050baa1ad3e7981f40b314dd59633e3afe41c55b
Dillon Corkran <dcorkran972@gmail.com>
2017-04-03 18:22:13 -0600

    :sparkles: refactored spotAd

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -6,12 +6,30 @@ import SpotTextLeft from './SpotTextLeft';
 
 
 class HomePage extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      spotInfo1 : {
+        header:"Ad Buying Power at Scale",
+        info:"Real-time programmatic advertising platform built for simple operation, yet powerful enough to handle campaigns of any size.",
+        light:"Find out why Choozle is the world's fastest growing digital marketing platform."
+      },
+      spotInfo2 : {
+        header:"Flexible Ad Buying Platform",
+        info:"No minimum ad spend. Unlimited advertiser accounts. Simple pay-as-you-go subscriptions for ad buying and data products. Self-serve or fully-managed.",
+        light:"The only platform that blends power and sophistication with simple operation."
+      }
+    };
+  }
   render() {
     return (
       <div className="homepage-bg">
         <Jumbotron className="jumbo"/>
         <JumboLabel/>
-        <SpotTextLeft/>
+        <SpotTextLeft
+          spotInfo={this.state.spotInfo1}/>
+        <SpotTextLeft
+          spotInfo={this.state.spotInfo2}/>
         <h1>sss</h1>
         <h1>sss</h1>
         <h1>sss</h1>
--- src/components/home/SpotTextLeft.js
+++ src/components/home/SpotTextLeft.js
@@ -1,21 +1,23 @@
 import React, {PropTypes} from 'react';
 import { Parallax } from 'react-parallax';
 
-const SpotTextLeft = () => {
+const SpotTextLeft = ({spotInfo}) => {
   return (
     <div className="container spot-container">
       <div className="row">
         <div className="col-xs-4 col-xs-offset-1">
-          <h3>Ad Buying Power at Scale</h3>
-          <p>Real-time programmatic advertising platform built for simple operation, yet powerful enough to handle campaigns of any size.</p>
-          <p>Find out why Choozle is the world's fastest growing digital marketing platform.</p>
+          <h3 className="spot-header-text">{spotInfo.header}</h3>
+          <p className="spot-info-text">{spotInfo.info}</p>
+          <p className="spot-light-text">{spotInfo.light}</p>
         </div>
         <div className="col-xs-6 spot-img-container">
           <img className="power-at-scale-img" src={require('../../img/PowerAtScale.png')}></img>
         </div>
       </div>
+      <hr></hr>
     </div>
-  )
-}
+  );
+};
 
-export default SpotTextLeft
+
+export default SpotTextLeft;
