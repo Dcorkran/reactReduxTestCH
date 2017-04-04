27f54f254328f5ca5aa6892fb62cba7afcd652f0
Dillon Corkran <dcorkran972@gmail.com>
2017-04-04 12:02:18 -0600

    :sparkles: finished

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -3,6 +3,11 @@ import {Link} from 'react-router';
 import Jumbotron from './Jumbotron';
 import JumboLabel from './JumboLabel';
 import SpotTextLeft from './SpotTextLeft';
+import SpotTextRight from './SpotTextRight';
+import FlexPlatImg from '../../img/flexplat.png';
+import PowerAtScaleImg from '../../img/PowerAtScale.png';
+import Support from '../../img/support.png';
+import DataImg from '../../img/data.png';
 
 
 class HomePage extends React.Component {
@@ -12,12 +17,26 @@ class HomePage extends React.Component {
       spotInfo1 : {
         header:"Ad Buying Power at Scale",
         info:"Real-time programmatic advertising platform built for simple operation, yet powerful enough to handle campaigns of any size.",
-        light:"Find out why Choozle is the world's fastest growing digital marketing platform."
+        light:"Find out why Choozle is the world's fastest growing digital marketing platform.",
+        img:PowerAtScaleImg
       },
       spotInfo2 : {
         header:"Flexible Ad Buying Platform",
         info:"No minimum ad spend. Unlimited advertiser accounts. Simple pay-as-you-go subscriptions for ad buying and data products. Self-serve or fully-managed.",
-        light:"The only platform that blends power and sophistication with simple operation."
+        light:"The only platform that blends power and sophistication with simple operation.",
+        img:FlexPlatImg
+      },
+      spotInfo3 : {
+        header:"Superior Support & Quick Set-Up",
+        info:"Take the wheel and drive yourself in no time. We will ride shotgun as long as you need. Unlimited training, webinars and video courses.",
+        light:"Say no to the black box—unparalleled transparency and control in both self-service and fully-managed mode.",
+        img:Support
+      },
+      spotInfo4 : {
+        header:"Premium Data at Your Fingertips",
+        info:"Access to 50+ premium data providers who represent over 100,000 targeting segments, including website visitor and CRM matching.",
+        light:"Effective and efficient targeting for ad campaigns of any size.",
+        img:DataImg
       }
     };
   }
@@ -28,21 +47,12 @@ class HomePage extends React.Component {
         <JumboLabel/>
         <SpotTextLeft
           spotInfo={this.state.spotInfo1}/>
+        <SpotTextRight
+          spotInfo={this.state.spotInfo4}/>
         <SpotTextLeft
           spotInfo={this.state.spotInfo2}/>
-        <h1>sss</h1>
-        <h1>sss</h1>
-        <h1>sss</h1>
-        <h1>sss</h1>
-        <h1>sss</h1>
-        <h1>sss</h1>
-        <h1>sss</h1>
-        <h1>sss</h1>
-        <h1>sss</h1>
-        <h1>sss</h1>
-        <h1>sss</h1>
-        <h1>sss</h1>
-        <h1>sss</h1>
+        <SpotTextRight
+          spotInfo={this.state.spotInfo3}/>
       </div>
     );
   }
--- src/components/home/SpotTextRight.js
+++ src/components/home/SpotTextRight.js
@@ -1,17 +1,17 @@
 import React, {PropTypes} from 'react';
 import { Parallax } from 'react-parallax';
 
-const SpotTextLeft = ({spotInfo}) => {
+const SpotTextRight = ({spotInfo}) => {
   return (
     <div className="container spot-container">
       <div className="row">
-        <div className="col-xs-4 col-xs-offset-1">
+        <div className="col-xs-6 col-xs-offset-1 spot-img-container">
+          <img className="power-at-scale-img" src={spotInfo.img}></img>
+        </div>
+        <div className="col-xs-4">
           <h3 className="spot-header-text">{spotInfo.header}</h3>
           <p className="spot-info-text">{spotInfo.info}</p>
           <p className="spot-light-text">{spotInfo.light}</p>
-        </div>
-        <div className="col-xs-6 spot-img-container">
-          <img className="power-at-scale-img" src={spotInfo.img}></img>
         </div>
       </div>
       <hr></hr>
@@ -19,8 +19,8 @@ const SpotTextLeft = ({spotInfo}) => {
   );
 };
 
-SpotTextLeft.propTypes = {
+SpotTextRight.propTypes = {
    spotInfo: React.PropTypes.object.isRequired
 };
 
-export default SpotTextLeft;
+export default SpotTextRight;
